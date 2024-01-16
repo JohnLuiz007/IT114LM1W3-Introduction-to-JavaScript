79426fec0a1060efb87ddc7104384d0bd89a94d3
JohnLuiz007 <115198373+JohnLuiz007@users.noreply.github.com>
2024-01-16 23:52:53 +0800

    Update index.js

--- 5. Map, Filter, and Reduce/index.js
+++ 5. Map, Filter, and Reduce/index.js
@@ -1,26 +1,38 @@
-/*
-    Resources:
-    - freeCodeCamp on Map, Reduce and Filter: https://www.freecodecamp.org/news/javascript-map-reduce-and-filter-explained-with-examples/
-*/
 // TODO 5.1 Use the map method to create a new array that contains the squares of all numbers in the array
-const numbers = [1, 2, 4, 4, 5];
-// Your code here
+const inputValues = [3, 5, 7, 2, 8];
+const squaredValues = inputValues.map((value) => value * value);
+console.log(squaredValues);
+
 
 // TODO 5.2 Use the filter method to create a new array that contains only the even numbers in the array
-// Your code here
+const originalArray = [3, 5, 7, 2, 8];
+const filteredEvenNumbers = originalArray.filter((value) => value % 2 === 0);
+console.log("Filtered even numbers: " + filteredEvenNumbers);
 
 // TODO 5.3 Use the reduce method to find the sum of all numbers in the array
-// Your code here
+const arrayToCalculate = [3, 5, 7, 2, 8];
+const resultSum = arrayToCalculate.reduce((runningTotal, currentValue) => runningTotal + currentValue, 0);
+console.log("The sum of the values: " + resultSum);
+
 
 // TODO 5.4 Use the map method to convert all elements in the array to uppercase
-const words = ['hello', 'world'];
-// Your code here
+const inputWord = ['hello', 'world'];
+const capitalPhrases = inputWord.map((phrase) => phrase.toUpperCase());
+console.log(capitalPhrases);
+
+
 
 // TODO 5.5 Use the filter method to create a new array that contains only the words with more than four letters in the array
-// Your code here
+const champions = ["Syndra", "Zed", "Tempest", "Yasuo"];
+const filteredChampions = champions.filter((champion) => champion.length > 4);
+console.log(filteredChampions);
+
 
 // TODO 5.6 Use the reduce method to concatenate all words in the array into a single string
-// Your code here
-
+const resultString = customArray.reduce((combined, item) => combined + item, '');
+console.log(resultString);
 // Checkpoint 5.1 Summarize what map, filter, and reduce do
 // Answer: 
+// map transforms each element of an array, filter selectively includes elements 
+//based on a condition, and reduce iterates
+// over array elements to accumulate a single result using a specified function
